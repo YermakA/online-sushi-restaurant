aa21b2cc7332a29a4ac5dad7324404424a191207
YermakA <ermak.080@mail.ru>
2023-07-28 19:43:58 +0800

    components created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./components/layout/Header.jsx";
-
+import Meals from "./components/meals/Meals.jsx"
 
 function App() {
   return (
     <React.Fragment>
       < Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
