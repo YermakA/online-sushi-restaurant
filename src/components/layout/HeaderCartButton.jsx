848fdd7e8ec6f4ef1c924e930c536efd2f2b073c
YermakA <ermak.080@mail.ru>
2023-07-29 23:21:27 +0800

    useEffect added

--- src/components/layout/HeaderCartButton.jsx
+++ src/components/layout/HeaderCartButton.jsx
@@ -1,6 +1,6 @@
 import styles from './HeaderCartButton.module.css'
 import CartIcon from './../cart/CartIcon';
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
@@ -9,9 +9,21 @@ const HeaderCartButton = (props) => {
     return currentValue + item.amount
   }, 0)
 
+  const [animatedClass, setAnimatedClass] = useState(false)
+  useEffect(() => {
+    if (cartContext.items.length === 0) {
+      return
+    }
+    setAnimatedClass(true)
+    const timer = setTimeout(() => { setAnimatedClass(false) }, 300)
 
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [cartContext.items])
+  const buttonClasses = `${styles.button} ${animatedClass && styles.bump}`
   return (
-    <button className={styles.button} onClick={props.showCart}>
+    <button className={buttonClasses} onClick={props.showCart} >
       <span className={styles.icon}><CartIcon /></span>
       <span >Корзина</span>
       <span className={styles.badge}>{cartItemsNumber}</span>
